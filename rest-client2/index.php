783219ddf30b32a4fc816b04dd5ec377e53343b7
Tria2025 <triasilviana6@gmail.com>
2025-10-21 16:00:42 +0700

    rest-client

--- rest-client2/index.php
+++ rest-client2/index.php
@@ -56,7 +56,7 @@ $hasil=json_decode($data,true);
               <img src="<?php echo $row['urlToImage']; ?>" class="card-img-top" height="180px">
               <div class="card-body">
                 <p class="card-text"><i>Oleh <?php echo $row['author']; ?></i> ~ <?php echo $row['title']; ?></p>
-                <p class="text-right"><a href="<?php echo $row['url']; ?>" target="_blank">Selengkapnya..</a></p>
+                <p class="text-right"><a href="<?php echo $row['']; ?>" target="_blank">Selengkapnya..</a></p>
               </div>
             </div>
         </div>
@@ -73,3 +73,4 @@ $hasil=json_decode($data,true);
 </body>
 
 </html>
+
